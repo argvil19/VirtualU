60470973056da2607b9b521506779a2fdb3e1831
quangogster <quangogster@gmail.com>
2016-12-18 17:44:03 -0800

    rename dir

--- SERVER/public/controllers/grades-ctrl.js
+++
@@ -1,4 +0,0 @@
-app.controller('GradesCtrl', ['$scope', 'CurrentUser', function($scope, CurrentUser, $http) {
-
-   $scope.user = CurrentUser.currentUser
-}]);
--- SERVER/routes/index.js
+++
@@ -1,9 +0,0 @@
-const octaverRoutes = require('./octaver');
-const quizRoutes = require('./quizzes');
-const authRoutes = require('./auth-routes');
-
-module.exports = (app) => {
-  octaverRoutes(app);
-  quizRoutes(app);
-  authRoutes(app);
-};
