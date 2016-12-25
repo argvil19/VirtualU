19fe896a31c2aa45a61e3a1a4244e77c47740ced
Argenis Villasmil <argvil19@gmail.com>
2016-12-25 06:56:54 +0000

    Added GET /API/user/profile

--- src/API/routes/users.js
+++ src/API/routes/users.js
@@ -1,7 +1,18 @@
+const getProfile = require('../controllers/profile/get-profile');
 const updateProfile = require('../controllers/profile/update-profile');
 const checkAuth = require('../helpers/auth/check-auth');
 
 module.exports = (app) => {
+  app.get('/API/user/profile/', checkAuth, (req, res, next) => {
+    getProfile(req.user, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+
+      return res.status(data.status).send(data);
+    })
+  });
+
   app.put('/API/user/profile/', checkAuth, (req, res, next) => {
     if (!(req.body.email || req.body.name || req.body.birthday)) {
       return next({
