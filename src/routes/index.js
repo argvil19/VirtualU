d727ac9c85f62d65e94fd637e8b18d8be29dd624
Argenis Villasmil <argvil19@gmail.com>
2017-01-06 03:13:45 +0000

    added error middleware. Fix gitlab-ci

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,20 +17,20 @@
  * See the Express application routing documentation for more information:
  * http://expressjs.com/api.html#app.VERB
  */
- 
+
 import React from 'react';
 import ReactDom from 'react-dom/server';
 import {
-  match
+	match
 }
 from 'react-router';
 import {
-  Provider
+	Provider
 }
 from 'react-redux';
 import {
-  ReduxAsyncConnect,
-  loadOnServer
+	ReduxAsyncConnect,
+	loadOnServer
 }
 from 'redux-connect';
 import configureStore from '../redux/configureStore';
@@ -54,57 +54,64 @@ var routes = {
 };
 
 // Setup Route Bindings
-exports = module.exports = function (app) {
+exports = module.exports = function(app) {
 	// Views
 	app.get('/', routes.views.index);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
-	
+
 	hvuRoutes(app);
-	
+
 	app.use((req, res, next) => {
-	  const store = configureStore();
-	
-	  const state = store.getState();
-	
-	  // This setting is required for material-ui server-side rendering
-	  state.theme.userAgent = req.headers['user-agent'];
-	
-	  match({
-	    routes,
-	    location: req.url
-	  }, (error, redirectLocation, renderProps) => {
-	    if (redirectLocation) {
-	      return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
-	    }
-	
-	    if (error) {
-	      return next({
-	        message: error.message,
-	        status: 500,
-	        success: false
-	      });
-	    }
-	
-	    if (!renderProps) {
-	      return next({
-	        message: 'Not found',
-	        status: 404,
-	        success: false
-	      });
-	    }
-	
-	    loadOnServer({...renderProps,
-	      store
-	    }).then(() => {
-	      const componentHTML = ReactDom.renderToString(
-	        <Provider store={store} key="provider">
+		const store = configureStore();
+
+		const state = store.getState();
+
+		// This setting is required for material-ui server-side rendering
+		state.theme.userAgent = req.headers['user-agent'];
+
+		match({
+			routes,
+			location: req.url
+		}, (error, redirectLocation, renderProps) => {
+			if (redirectLocation) {
+				return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
+			}
+
+			if (error) {
+				return next({
+					message: error.message,
+					status: 500,
+					success: false
+				});
+			}
+
+			if (!renderProps) {
+				return next({
+					message: 'Not found',
+					status: 404,
+					success: false
+				});
+			}
+
+			loadOnServer({...renderProps,
+				store
+			}).then(() => {
+				const componentHTML = ReactDom.renderToString(
+					<Provider store={store} key="provider">
 	          <ReduxAsyncConnect {...renderProps} />
 	        </Provider>
-	      );
-	      res.send(renderHTML(componentHTML, store.getState(), assetUrl));
-	    });
-	  });
+				);
+				res.send(renderHTML(componentHTML, store.getState(), assetUrl));
+			});
+		});
+	});
+
+	app.use(function(error, req, res, next) {
+		res.status(error.status).send({
+			message: error.message,
+			success: false,
+		});
 	});
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
