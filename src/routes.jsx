8f3c5ec0c259a84a82bb214bda4a50227d020b7a
Shturman <mrshturman@gmail.com>
2017-01-07 01:41:07 +0700

    Fixed spaces syntax

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,13 +10,13 @@ import App                          from './components/App';
 import Homepage                     from './components/Homepage';
 
 const Routes = (
-	<Router render={(props) => <ReduxAsyncConnect {...props}/>} history={browserHistory}>
-		<Route path='/' component={App}>
-			<IndexRoute component={Homepage} />
-			<Route path='/quizes' />
-			<Route path='/quize/:id' />
-		</Route>
-	</Router>
+  <Router render={(props) => <ReduxAsyncConnect {...props}/>} history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Homepage} />
+      <Route path='/quizes' />
+      <Route path='/quize/:id' />
+    </Route>
+  </Router>
 );
 
 export default Routes;
